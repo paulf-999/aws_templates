549fa75f5afbb8ed704bb07409f295ff1f66869e
Paul Fry <paul.fry@versent.com.au>
2021-05-25 21:13:12 +1000

    git repo housekeeping / baselined

---
+++ mwaa-aws-managed-airflow-v1/simple-single-cfn/dags/secrets-manager.py
@@ -0,0 +1,41 @@
+from airflow import DAG, settings, secrets
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.contrib.hooks.aws_hook import AwsHook
+
+from datetime import timedelta
+import os
+
+### The steps to create this secret key can be found at: https://docs.aws.amazon.com/mwaa/latest/userguide/connections-secrets-manager.html
+sm_secretId_name = 'airflow/connections/mwaa-connection'
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+    'depends_on_past': False
+}
+
+
+### Gets the secret myconn from Secrets Manager
+def read_from_aws_sm_fn(**kwargs):
+    ### set up Secrets Manager
+    hook = AwsHook()
+    client = hook.get_client_type('secretsmanager')
+    response = client.get_secret_value(SecretId=sm_secretId_name)
+    myConnSecretString = response["SecretString"]
+
+    return myConnSecretString
+
+### 'os.path.basename(__file__).replace(".py", "")' uses the file name secrets-manager.py for a DAG ID of secrets-manager
+with DAG(
+        dag_id=os.path.basename(__file__).replace(".py", ""),
+        default_args=default_args,
+        dagrun_timeout=timedelta(hours=2),
+        start_date=days_ago(1),
+        schedule_interval=None
+) as dag:
+    write_all_to_aws_sm = PythonOperator(
+        task_id="read_from_aws_sm",
+        python_callable=read_from_aws_sm_fn,
+        provide_context=True
+    )
