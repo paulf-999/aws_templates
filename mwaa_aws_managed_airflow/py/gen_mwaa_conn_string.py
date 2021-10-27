91c563562dff4d0a052c980df0824c495fdc657b
Paul Fry <paul-fry@hotmail.com>
2021-10-27 17:33:27 +1100

    housekeeping

--- mwaa_aws_managed_airflow/py/gen_mwaa_conn_string.py
+++ mwaa_aws_managed_airflow/py/gen_mwaa_conn_string.py
@@ -16,42 +16,37 @@ from datetime import datetime
 from time import time
 import logging
 
-#import custom modules
+# import custom modules
 import urllib.parse
 
 # Set up a specific logger with our desired output level
-logging.basicConfig(format='%(message)s')
-logger = logging.getLogger('application_logger')
+logging.basicConfig(format="%(message)s")
+logger = logging.getLogger("application_logger")
 logger.setLevel(logging.INFO)
 # by default, turn off log outputs. But if desired, change this arg to True
-#logger.propagate = False
+# logger.propagate = False
 
 current_dt_obj = datetime.now()
-current_date_str = current_dt_obj.strftime('%d-%m-%Y')
-current_time_str = current_dt_obj.strftime('%H:%M:%S')
-#can use 'current_dt_obj' to get other date parts. E.g. 'current_dt_obj.year'
+current_date_str = current_dt_obj.strftime("%d-%m-%Y")
+current_time_str = current_dt_obj.strftime("%H:%M:%S")
+# can use 'current_dt_obj' to get other date parts. E.g. 'current_dt_obj.year'
+
 
 def main():
-    """ Main entry point of the app """
+    """Main entry point of the app"""
     START_TIME = time()
-    logger.debug(f"Function called: main()")
-    
-    #conn_type = 'YOUR_DB_OPTION'
-    #host = 'YOUR_MWAA_AIRFLOW_UI_URL'
-    #login = 'YOUR_AWS_ACCESS_KEY_ID'
-    #password = 'YOUR_AWS_SECRET_ACCESS_KEY'
-    #role_arn = urllib.parse.quote_plus('YOUR_EXECUTION_ROLE_ARN')
-    #region_name = 'YOUR_REGION'
+    logger.debug("Function called: main()")
 
-    conn_string = '{0}://{1}:{2}@{3}?role_arn={4}&region_name={5}'.format(conn_type, airflow_ui_url, login, password, role_arn, region_name)
+    conn_string = "{0}://{1}:{2}@{3}?role_arn={4}&region_name={5}".format(conn_type, airflow_ui_url, login, password, role_arn, region_name)
     print(conn_string)
 
     logger.debug(f"Function finished: main() finished in {round(time() - START_TIME, 2)} seconds")
 
     return
 
-if __name__ == '__main__':
-    """ This is executed when run from the command line """
+
+if __name__ == "__main__":
+    """This is executed when run from the command line"""
     conn_type = sys.argv[1]
     airflow_ui_url = sys.argv[2]
     login = sys.argv[3]
@@ -60,14 +55,7 @@ if __name__ == '__main__':
     region_name = sys.argv[6]
     aws_profile = sys.argv[7]
 
-    #print(f"conn_type = {conn_type}")
-    #print(f"airflow_ui_url = {airflow_ui_url}")
-    #print(f"login = {login}")
-    #print(f"password = {password}")
-    #print(f"role_arn = {role_arn}")
-    #print(f"region_name = {region_name}")
-
-    conn_string = '{0}://{1}:{2}@{3}?role_arn={4}&region_name={5}'.format(conn_type, airflow_ui_url, login, password, role_arn, region_name)
+    conn_string = "{0}://{1}:{2}@{3}?role_arn={4}&region_name={5}".format(conn_type, airflow_ui_url, login, password, role_arn, region_name)
     print(conn_string)
 
-    #main()
+    main()
